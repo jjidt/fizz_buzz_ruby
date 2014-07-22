02c88ddf04cff21e1ad9fdc24bdbb079bbdf81f2
J.J. Idt & Tanner Steward <student@epicodus.com>
2014-07-22 11:06:02 -0700

    make the fizz_buzz function return buzz for numbers divisible by 5

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,7 +1,9 @@
 def fizz_buzz(number)
   if (number % 3 == 0)
     "fizz"
-  elsif (number % 3 != 0)
+  elsif (number % 5 == 0)
+    "buzz"
+  else (number % 3 != 0)
     number
   end
 end
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -2,11 +2,14 @@ require('rspec')
 require('fizz_buzz')
 
 describe('fizz_buzz') do
-  it("returns fizz for every number divisable by three") do
+  it("returns fizz for every number divisible by three") do
     fizz_buzz(3).should(eq('fizz'))
   end
-  it("returns the number if it is not divisable by three") do
+  it("returns the number if it is not divisible by three") do
     fizz_buzz(4).should(eq(4))
+  end
+  it("returns buzz for every number divisible by five") do
+    fizz_buzz(5).should(eq('buzz'))
   end
 end
 
